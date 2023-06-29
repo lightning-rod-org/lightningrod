55955b6fa6a84f083aac147587be4aab3a2c00ad
james-latimer <jameslatimer@knights.ucf.edu>
2023-06-29 03:05:19 -0400

    Added ip as a field
    Ip address of the client making the request is now stored in the database as an attribute of the model

--- api/api/settings.py
+++ api/api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'parserAPI',
     'rest_framework',
+    'ipware'
 ]
 
 MIDDLEWARE = [
--- api/parserAPI/serializers.py
+++ api/parserAPI/serializers.py
@@ -3,4 +3,4 @@ from .models import parseInput
 class InputSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = parseInput
-        fields = ['ticket_number', 'p_input', 'time_created', 'time_finished', 'p_output']
+        fields = ['ticket_number', 'client_ip', 'p_input', 'time_created', 'time_finished', 'p_output']
