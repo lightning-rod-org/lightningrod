883e67f793d1ac07619ca53650799325676ac93a
james-latimer <jameslatimer@knights.ucf.edu>
2023-06-30 12:28:34 -0400

    Changed around some methods

--- api/parserAPI/tests.py
+++ api/parserAPI/tests.py
@@ -3,6 +3,13 @@ from .views import instantParse
 from .models import parseInput
 from django.urls import reverse
 from django.utils import timezone
+<<<<<<< Updated upstream
+=======
+from django.views import addParse
+import subprocess
+from django.http import HttpRequest
+from parserAPI.views import addParse
+>>>>>>> Stashed changes
 
 class Test(TestCase):
     def test_does_not_exist(self):
@@ -23,117 +30,28 @@ class Test(TestCase):
        self.assertEqual(response.status_code, 200)
        obj = parseInput.objects.first()
        self.assertIsNotNone(obj.time_created)
+<<<<<<< Updated upstream
 
+=======
+        
+   def test_add_parse_fields(self):
+        # Create a dummy HTTP POST request
+        request = HttpRequest()
+        request.method = 'POST'
+        request.META['REMOTE_ADDR'] = '127.0.0.1'
+        # Add any other necessary request parameters
+>>>>>>> Stashed changes
 
-    #TODO 
+        # Perform the view function call
+        response = addParse(request)
 
-#    def test_add_parse(self):
-#         # Create a POST request
-#         data = {
-#             'p_input': 'example input',
-#         }
-#         request = self.factory.post(reverse(addParse), data=data)
-        
-#         # Mock the subprocess check_output result
-#         subprocess_result = "example output"
-#         subprocess_mock = subprocess.Mock(return_value=subprocess_result)
-#         with subprocess.patch('subprocess.check_output', subprocess_mock):
-#             # Make the request to the view
-#             response = addParse(request)
-        
-#         # Assert the response status code
-#         self.assertEqual(response.status_code, 201)
-        
-#         # Assert that the object was saved to the database
-#         self.assertEqual(parseInput.objects.count(), 1)
-        
-#         # Assert the object's attributes
-#         obj = parseInput.objects.first()
-#         self.assertEqual(obj.p_input, 'example input')
-#         self.assertIsNotNone(obj.time_created)
-#         #this is gonna need to be changed later once the time_finished gets 
-#         #properly added
-#         self.assertIsNone(obj.time_finished)
-#         self.assertEqual(obj.p_output, subprocess_result)
-
-
-'''
-from django.test import TestCase, RequestFactory
-from django.urls import reverse
-from rest_framework.parsers import JSONParser
-import subprocess
-from .views import addParse
-from .models import parseInput
-import subprocess
-
-class AddParseTestCase(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-    
-    def test_add_parse(self):
-        # Create a POST request
-        data = {
-            'p_input': 'example input',
-        }
-        request = self.factory.post(reverse(addParse), data=data)
-        
-        # Mock the subprocess check_output result
-        subprocess_result = "example output"
-        subprocess_mock = subprocess.Mock(return_value=subprocess_result)
-        with subprocess.patch('subprocess.check_output', subprocess_mock):
-            # Make the request to the view
-            response = addParse(request)
-        
-        # Assert the response status code
+        # Assert that the response status code is 201 (Created)
         self.assertEqual(response.status_code, 201)
 
-from rest_framework import status
-from rest_framework.test import APITestCase
-import json
-        
-        # Assert that the object was saved to the database
-        self.assertEqual(parseInput.objects.count(), 1)
-        
-        # Assert the object's attributes
-        obj = parseInput.objects.first()
-        self.assertEqual(obj.p_input, 'example input')
-        self.assertIsNotNone(obj.time_created)
-        #this is gonna need to be changed later once the time_finished gets 
-        #properly added
-        self.assertIsNone(obj.time_finished)
-        self.assertEqual(obj.p_output, subprocess_result)
-class Test(APITestCase):
-    def test_does_not_exist(self):
-        #response = self.client.post("/test/", {"title": "new idea"}, format="json")
-        response = self.client.get("/api/instantParse/test")
-        self.assertEqual(response.status_code, 404)
+        # Assert that the parsed input fields have the expected values
+        data = response.json()
+        self.assertEqual(data['ticket_number'], parseInput.objects.count() + 1)
+        self.assertEqual(data['client_ip'], '127.0.0.1')
+        # Add assertions for other fields as needed
 
-    def test_exists(self):
-        message = {"message": "www.dog.com"}
-        response = self.client.get("/api/instantParse/", message)
-        self.assertEqual(response.status_code, 200)
-
-    
-    def test_post_success(self):
-        data = {'title': 'hello'}
-        response = self.client.post("/api/instantParse/", data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-
-    def test_post_error(self):
-        response = self.client.post("/api/instantParse/test", {"title": "hi"}, format="json")
-        self.assertEqual(response.status_code, 404)
-
-    def test_exist_and_message(self):
-        #response = self.client.post("/api/instantParse/", {"title": "new idea"}, format="json")
-        response = self.client.get("/api/instantParse/?message='yes'")
-        self.assertEqual(response.status_code, 200)
-
-        x = json.loads(response.content)
-        
-        self.assertEqual(json.loads(response.content))
-
-        #print(json.loads(response.content))
-    '''
 
--- api/parserAPI/views.py
+++ api/parserAPI/views.py
@@ -18,26 +18,36 @@ from django.http import HttpResponse
 
 
 @csrf_exempt
+@api_view(['GET'])
 def instantParse(request):
     '''
     List all task snippets
     '''
+<<<<<<< Updated upstream
     if request.method == 'GET':
+=======
+    if(request.method == 'GET'):
+>>>>>>> Stashed changes
         data = JSONParser().parse(request)
         data['ticket_number'] = parseInput.objects.count() + 1
+        data['client_ip'] = request.META.get('REMOTE_ADDR')
         data['time_created'] = timezone.now()
-        data['time_finished'] = None  
         message = data['p_input']
         command = "dig " + message + " | jc --dig"
+        data['time_finished'] = timezone.now()  
         data['p_output'] = subprocess.check_output(command, shell=True, text=True)
+        
+        
         serializer = InputSerializer(data=data)
         # check if the sent information is okay
         if(serializer.is_valid()):
             # if okay, save it on the database
+            serializer.save()
             # provide a Json Response with the data that was saved
             return JsonResponse(serializer.data, status=201)
             # provide a Json Response with the necessary error information
         return JsonResponse(serializer.errors, status=400)
+
          
 @csrf_exempt
 @api_view(['POST'])
