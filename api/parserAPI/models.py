f014683d890100596405f43e7dfadbaf5a351c97
Julian Jackson <jackson.julian55@yahoo.com>
2023-07-20 10:08:29 -0400

    Changed ticet number from incrementing integer to random uuid

--- api/parserAPI/models.py
+++ api/parserAPI/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class Ticket(models.Model):
-    ticket_number = models.IntegerField(primary_key=True)
+    ticket_number = models.CharField(max_length=36)
     parser = models.CharField(max_length=100)
     status = models.CharField(max_length=100, default='Starting')
 
@@ -10,12 +11,14 @@ class Ticket(models.Model):
         self.status = status
         self.save(update_fields=['status'])
 
+
 class AdditionalFields(models.Model):
     ticket = models.OneToOneField(Ticket, on_delete=models.CASCADE, related_name='additional_fields')
     client_ip = models.CharField(max_length=100)
     time_created = models.DateTimeField()
     time_finished = models.DateTimeField()
     p_output = models.JSONField()
+
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
