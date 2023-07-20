83e57d83519dc91a518f2009eae316c5bf0b14d4
solomonleang <124934439+solomonleang@users.noreply.github.com>
2023-07-20 09:55:50 -0400

    Asynchronous functionality (#4)
    * Updating Post and Get functions for async
    
    * restructured post and get requests!
    
    * Added threads and some async functionality.
    
    * Removed background task import statements
    
    * Added more comments, and cleaned up some code.
    
    * Using separate links now for GET and POST.
    
    * Cleaned up file and fixed time.

--- api/parserAPI/models.py
+++ api/parserAPI/models.py
@@ -1,23 +1,21 @@
 from django.db import models
-from datetime import datetime
-
 
 # Create your models here.
+class Ticket(models.Model):
+    ticket_number = models.IntegerField(primary_key=True)
+    parser = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, default='Starting')
 
-class parseInput(models.Model):
-    # title 
-    ticket_number = models.IntegerField()
-    client_ip = models.CharField(max_length=50)
-    time_created = models.DateTimeField(null=True, blank=True)
-    time_finished = models.DateTimeField(null=True, blank=True)
-    parser = models.CharField(max_length=50)
+    def update_status(self, status):
+        self.status = status
+        self.save(update_fields=['status'])
+
+class AdditionalFields(models.Model):
+    ticket = models.OneToOneField(Ticket, on_delete=models.CASCADE, related_name='additional_fields')
+    client_ip = models.CharField(max_length=100)
+    time_created = models.DateTimeField()
+    time_finished = models.DateTimeField()
     p_output = models.JSONField()
-
-    def __str__(self):
-        return self.parser
-
-    
-
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
