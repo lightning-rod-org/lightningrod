7c6191e3772acf27d790cf4b158fb83a3eb511a1
Solomon Leang <solomonleang@gmail.com>
2023-07-26 15:34:38 -0400

    100% Coverage for Views!

--- api/parserAPI/views.py
+++ api/parserAPI/views.py
@@ -44,14 +44,14 @@ def instantParse(request):
                 return JsonResponse(response_data, status=200)
         # Error in finding ticket number.
         except (Ticket.DoesNotExist, AdditionalFields.DoesNotExist):
-            return JsonResponse({'error': 'Ticket not found'}, status=404)
+            return JsonResponse({'error': 'Ticket Not Found'}, status=404)
 
 
 # Helper function to parse the data for extra fields.
-def parseData(request, file_content, passed_ticket):
+def parseData(file_content, passed_ticket):
     additional_fields = AdditionalFields(ticket=passed_ticket, time_created=timezone.now(),
                                          time_finished=timezone.now())
-    additional_fields.client_ip = request.META.get("REMOTE_ADDR")
+    additional_fields.client_ip = "PLACEHOLDER IP"
     additional_fields.ticket.update_status("In Progress")
 
     assert isinstance(file_content, str)
@@ -69,8 +69,6 @@ def parseData(request, file_content, passed_ticket):
     if serializer.is_valid():
         print("Data saved!")
         additional_fields.save()
-    else:
-        print("Not valid!")
 
 
 @csrf_exempt
@@ -79,7 +77,7 @@ def addParse(request):
     if request.method == 'POST':
         data = request.data
         if not jc.parser_mod_list().__contains__(data.get('parser')):
-            return JsonResponse({"Error": "Invalid Parser Type"}, status=400)
+            return JsonResponse({"error": "Invalid Parser Type"}, status=400)
         ticket_number = str(uuid.uuid4())  # Get the next available ticket number
 
         # Create a ticket number with the status starting.
@@ -99,9 +97,7 @@ def addParse(request):
             ticket_serializer.save()
 
             # Create a new thread for each request.
-            thread = threading.Thread(target=parseData, args=(request, file_content, new_ticket), daemon=True)
+            thread = threading.Thread(target=parseData, args=(file_content, new_ticket), daemon=True)
             thread.start()
 
             return JsonResponse(ticket_serializer.data, status=201)
-        else:
-            return JsonResponse(ticket_serializer.errors, status=400)
