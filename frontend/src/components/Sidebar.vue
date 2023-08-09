af665370b86cbda61099ee5bf386df424891883d
james-latimer <james.latimer@blackhorsesolutions.com>
2023-08-09 14:01:04 -0400

    Update sidebar.vue

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -26,13 +26,13 @@
   <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
 </svg><span class="text-dark" href="http://google.com">  Admin</span>
         </a>
-        <a
+        <!-- <a
            href="#"
            class="list-group-item bg-light list-group-item-action py-3 ripple"
            ><i class="fas fa-lock fa-fw me-2 "></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 20 20">
   <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
 </svg><span class="text-dark">Password</span></a
-          >
+          > -->
           </div>
       </div>
   </nav>
